9acfd8d6028fccdb7322dbb72ce14e26bec1f2e5
Umeshkumar9414 <2015uec1428@mnit.ac.in>
2018-05-29 00:29:45 +0530

    Create Noble_Integer.Interview_Bit.java

---
+++ Noble_Integer.Interview_Bit.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solve(int[] a) {
+        int i,n=a.length;
+        Arrays.sort(a);
+        for(i=0;i<n;i++){
+            while(i+1<n && a[i]==a[i+1])
+            i++;
+            if(a[i]==(n-i-1))
+            return 1;
+        }
+        return -1;
+        
+        
+    }
+}
