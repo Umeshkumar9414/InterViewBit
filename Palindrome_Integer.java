c614a12546bab7bf89e50e19b6b3efb4f57e8e59
Umeshkumar9414 <2015uec1428@mnit.ac.in>
2018-06-06 21:54:37 +0530

    Update and rename Palindrome_Integer_Interview_Bit to Palindrome_Integer.java

--- Palindrome_Integer.java
+++ Palindrome_Integer.java
@@ -1,8 +1,9 @@
+//Question link
+//https://www.interviewbit.com/problems/palindrome-integer/
+//Determine whether an integer is a palindrome. Do this without extra space.
 
-Determine whether an integer is a palindrome. Do this without extra space.
-
-A palindrome integer is an integer x for which reverse(x) = x where reverse(x) is x with its digit reversed.
-Negative numbers are not palindromic.
+//A palindrome integer is an integer x for which reverse(x) = x where reverse(x) is x with its digit reversed.
+//Negative numbers are not palindromic.
 
 public class Solution {
     public int isPalindrome(int a) {
