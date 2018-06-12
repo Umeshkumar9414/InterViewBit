117e17bf39c7fc25c5649f0789c477cf48143ca7
Umeshkumar9414 <2015uec1428@mnit.ac.in>
2018-06-13 00:55:09 +0530

    Create Longest_common_prefix.java

---
+++ Strings/Longest_common_prefix.java
@@ -0,0 +1,23 @@
+//https://www.interviewbit.com/problems/longest-common-prefix/
+
+public class Solution {
+    public String longestCommonPrefix(ArrayList<String> a) {
+          int i,j,k,l;
+       // int n=a.length;
+        int n=a.size();
+        //String an=a[0];
+        String an=a.get(0);
+        k=an.length();
+        for(i=1;i<n;i++){
+            j=0;
+            l=a.get(i).length();
+            while(j<k && j<l && an.charAt(j)==a.get(i).charAt(j)){//a[i].charAt(j)
+                j++;
+            }
+            k=j;
+        }
+        
+        return an.substring(0,k);
+        
+    }
+}
