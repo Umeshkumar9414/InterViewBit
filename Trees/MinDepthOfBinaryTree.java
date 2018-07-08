041b31e6cc0c51fec9b954b03e13babc746a3ef7
Umesh <2015uec1428@mnit.ac.in>
2018-07-08 23:01:26 +0530

    Create MinDepthOfBinaryTree.java

---
+++ Trees/MinDepthOfBinaryTree.java
@@ -0,0 +1,25 @@
+//Que :- https://www.interviewbit.com/problems/min-depth-of-binary-tree/
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode A) {
+        if(A==null){
+            return Integer.MAX_VALUE;
+        }
+        if(A.left==null && A.right==null){
+            return 1;
+        }
+        return Math.min(minDepth(A.left),minDepth(A.right))+1;
+    }
+}
