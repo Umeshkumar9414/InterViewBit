bf28cc6318afeef8beb69703ee820ca681957583
Umesh <2015uec1428@mnit.ac.in>
2018-07-11 14:24:29 +0530

    Update InvertTheBinaryTree.java

--- Trees/InvertTheBinaryTree.java
+++ Trees/InvertTheBinaryTree.java
@@ -1,4 +1,3 @@
-//Que :- https://www.interviewbit.com/problems/invert-the-binary-tree/
 /**
  * Definition for binary tree
  * class TreeNode {
@@ -13,6 +12,14 @@
  * }
  */
 public class Solution {
-    public TreeNode invertTree(TreeNode A) {
+    public TreeNode invertTree(TreeNode root) {
+         if(root==null)
+            return root;
+        TreeNode tmp=root.left;
+        root.left=root.right;
+        root.right=tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
